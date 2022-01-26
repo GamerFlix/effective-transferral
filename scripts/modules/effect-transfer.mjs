d08b650411c87b615c4bd7ac6ecfa068422fe364
Flix <documentflix@gmail.com>
2022-01-26 02:09:18 +0100

    Added license information

--- scripts/modules/effect-transfer.mjs
+++ scripts/modules/effect-transfer.mjs
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the Effective Transferral module (https://github.com/GamerFlix/effective-transferral)
+ * Copyright (c) 2021 Felix Pohl.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 export class EffectTransfer{
 
     static NAME = "EffectTransfer";
