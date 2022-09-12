35c6b19f923bcc4546dbfd6a0664d6220aa579af
Flix <documentflix@gmail.com>
2022-09-12 22:32:22 +0200

    Changed comparison from _id to id. Closes #32

--- scripts/modules/effect-transfer.mjs
+++ scripts/modules/effect-transfer.mjs
@@ -17,18 +17,18 @@
 import { MODULE } from '../module.mjs'
 
 export class EffectTransfer {
-    
+
     static NAME = "EffectTransfer";
-    
+
     // Get the token document of a given actor
-    static tokenDocFromActor(actor){
+    static tokenDocFromActor(actor) {
         return actor?.token?.document ?? actor?.getActiveTokens(false, true)[0];
         // getActiveTokens(linked=false,document=false)
     }
 
     //Block transfer via a given method
-    static async setBlock(effect,boolean,type){
-        if ( effect?.parent?.parent ) {
+    static async setBlock(effect, boolean, type) {
+        if (effect?.parent?.parent) {
             ui.notifications.error(game.i18n.localize("ET.doubleEmbedded.error"));
             return undefined;
         }
@@ -36,81 +36,81 @@ export class EffectTransfer {
     }
 
     //Check whether transfer via a given method is possible
-    static getBlock(effect,type){
-        return !!foundry.utils.getProperty(effect,`flags.effective-transferral.transferBlock.${type}`)
+    static getBlock(effect, type) {
+        return !!foundry.utils.getProperty(effect, `flags.effective-transferral.transferBlock.${type}`)
     }
 
     // gets the value of the displayCard flag in transferBlock, returning false if undefined.
-    static getDisplayCardBlock(effect){
-        return EffectTransfer.getBlock(effect,"displayCard");
+    static getDisplayCardBlock(effect) {
+        return EffectTransfer.getBlock(effect, "displayCard");
     }
 
     // updates the flag object to include displayCard:boolean
-    static async setDisplayCardBlock(effect, boolean){
-        return EffectTransfer.setBlock(effect,boolean,"displayCard")
+    static async setDisplayCardBlock(effect, boolean) {
+        return EffectTransfer.setBlock(effect, boolean, "displayCard")
     }
-    
+
     // Gets the value of the chat flag in transferBlock, returning false if undefined
-    static getChatBlock(effect){
-        return EffectTransfer.getBlock(effect,"chat");
+    static getChatBlock(effect) {
+        return EffectTransfer.getBlock(effect, "chat");
     }
-    
+
     // Updates the flag object to include chat:boolean
-    static async setChatBlock(effect, boolean){
-        return EffectTransfer.setBlock(effect,boolean,"chat")
+    static async setChatBlock(effect, boolean) {
+        return EffectTransfer.setBlock(effect, boolean, "chat")
     }
-    
+
     // Gets the value of the button flag in transferBlock, returning false if undefined
-    static getButtonBlock(effect){
-        return EffectTransfer.getBlock(effect,"button");
+    static getButtonBlock(effect) {
+        return EffectTransfer.getBlock(effect, "button");
     }
-    
+
     // Updates the flag object to include button:boolean
-    static async setButtonBlock(effect, boolean){
-        return EffectTransfer.setBlock(effect,boolean,"button")
+    static async setButtonBlock(effect, boolean) {
+        return EffectTransfer.setBlock(effect, boolean, "button")
     }
-    
+
 
 
     // Toggleable console.log()
-    static async debug(){
-        if (MODULE.getSetting("debugMode")){
+    static async debug() {
+        if (MODULE.getSetting("debugMode")) {
             console.log(arguments);
         }
     }
-    
+
     // Returns a relevant function used to filter the given effect based on the specified type
     static isEligible = (type) => {
         switch (type) {
             case 'button': return (effect) =>
                 (effect.transfer === false || MODULE.getSetting("includeEquipTransfer")) &&
-                (!EffectTransfer.getBlock(effect,"button") && !MODULE.getSetting("neverButtonTransfer"));
+                (!EffectTransfer.getBlock(effect, "button") && !MODULE.getSetting("neverButtonTransfer"));
             case 'chat': return (effect) =>
                 (effect.transfer === false || MODULE.getSetting("includeEquipTransfer")) &&
-                (!EffectTransfer.getBlock(effect,"chat") && !MODULE.getSetting("neverChatTransfer"));
+                (!EffectTransfer.getBlock(effect, "chat") && !MODULE.getSetting("neverChatTransfer"));
             case 'displayCard': return (effect) =>
                 (effect.transfer === false || MODULE.getSetting("includeEquipTransfer")) &&
-                (!EffectTransfer.getBlock(effect,"displayCard") && !MODULE.getSetting("neverDisplayCardTransfer"));
+                (!EffectTransfer.getBlock(effect, "displayCard") && !MODULE.getSetting("neverDisplayCardTransfer"));
             default: return (_) => false;
         }
     }
-    
+
     // Takes an array of ActiveEffectObjects and bundles it so it can be passed to applyPackagedEffects / warpgate.mutate()
-    static packageEffects(validEffectsData){
+    static packageEffects(validEffectsData) {
         const aeData = validEffectsData.reduce((acc, ae) => {
             acc[ae.label] = ae;
             return acc;
         }, {});
-        
+
         EffectTransfer.debug("Prepared aeData");
-        
+
         /* Put effects into update object */
         return { embedded: { ActiveEffect: aeData } };
     }
-    
+
     //Takes a token doc, effects prepackaged by packageEffects and optionally an item name to apply effects
-    static async applyPackagedEffects(tokenDoc, packagedEffects, itemName = game.i18n.format("ET.applyEffect.defaultName")){
-        const comparisonKey=MODULE.getSetting("applyIdenticalEffects")?"_id":'label'
+    static async applyPackagedEffects(tokenDoc, packagedEffects, itemName = game.i18n.format("ET.applyEffect.defaultName")) {
+        const comparisonKey = MODULE.getSetting("applyIdenticalEffects") ? "id" : 'label'
         await warpgate.mutate(tokenDoc, packagedEffects, {}, {
             name: `Effective Transferral: ${itemName}`,
             description: game.i18n.format("ET.Dialog.Mutate.Description", {
@@ -118,68 +118,68 @@ export class EffectTransfer {
                 itemName,
                 tokenName: tokenDoc.name
             }),
-            comparisonKeys: { ActiveEffect:  comparisonKey},
-            permanent: MODULE.getSetting("permanentTransfer" )
+            comparisonKeys: { ActiveEffect: comparisonKey },
+            permanent: MODULE.getSetting("permanentTransfer")
         });
     }
-    
+
     // Delete mutations that no longer have the respective effect present
-    static async cleanUp(deletedEffect, context, userId){
+    static async cleanUp(deletedEffect, context, userId) {
         if (game.user.id !== userId) return;
-        
+
         EffectTransfer.debug("effect", deletedEffect);
         const tokenActor = deletedEffect?.parent;
-        
+
         // Verify it's actually an actor otherwise return
         if (!(tokenActor instanceof Actor)) return;
-        
+
         EffectTransfer.debug(tokenActor);
-        if(!tokenActor) return;
-        
+        if (!tokenActor) return;
+
         let tokenDoc = EffectTransfer.tokenDocFromActor(tokenActor);
         EffectTransfer.debug("Effect deletion happened on", tokenDoc);
-        
+
         let stack = warpgate.mutationStack(tokenDoc);
         EffectTransfer.debug("Mutationstack", stack);
-        
+
         stack.deleteAll((stack) => {
             // bail if the stack is not from ET
-            if ( !stack.name.includes("Effective Transferral: ")) return false;
-            
+            if (!stack.name.includes("Effective Transferral: ")) return false;
+
             // Bail if stack has no effects in it
             let stackEffectNames = Object.keys(stack.delta?.embedded?.ActiveEffect);
-            if ( !stackEffectNames ) return false;
-            
+            if (!stackEffectNames) return false;
+
             // Get the effect labels that are still on the token
             let presentEffectLabels = tokenDoc.actor.effects.map(i => i.label);
-            
+
             EffectTransfer.debug(presentEffectLabels);
-            
-            for (let stackEffectName of stackEffectNames){
+
+            for (let stackEffectName of stackEffectNames) {
                 // Check if label is inside effects on actor if yes bail
                 let value = presentEffectLabels.includes(stackEffectName);
                 if (value) return false;
             }
-            
+
             // If none of the effects inside the mutationstack are still present delete the stack
             return true;
         });
         await stack.commit();
     }
-    
+
     // pops up the dialogue and calls warpgate to apply effects
-    static async effectTransferDialogue(actor, tokenDoc, itemName, validEffectsData){
+    static async effectTransferDialogue(actor, tokenDoc, itemName, validEffectsData) {
         //Check whether we actually have effects on the item
-        if ( validEffectsData.length === 0 ){
+        if (validEffectsData.length === 0) {
             return; //If we do not have any, there is nothing to do
         }
-        
+
         // If we do not have any non-transfer effects there is nothing to do so exit
         let effect_target // initialiaze the variable again because scoping
-        
+
         /* If we have a token we are on a scene that uses tokens so we have stuff to target.
         Reflect that in the dialogue by giving the option to apply to targets */
-        if ( tokenDoc && actor ){
+        if (tokenDoc && actor) {
             effect_target = await warpgate.menu({
                 inputs: [{
                     label: `<center>
@@ -205,7 +205,7 @@ export class EffectTransfer {
         }
         /* if we do not have a token, we are either not on a scene or not on a scene
         that uses tokens (Theatre of the Mind) so there would not be anything to target anyhow */
-        else if ( actor ){
+        else if (actor) {
             effect_target = await warpgate.menu({
                 inputs: [{
                     label: `<center>
@@ -218,7 +218,7 @@ export class EffectTransfer {
                 buttons: [{
                     label: game.i18n.localize("ET.Dialog.Button.Self"),
                     value: "selfNoToken"
-                },{
+                }, {
                     label: game.i18n.localize("ET.Dialog.Button.None"),
                     value: "none"
                 }]
@@ -248,22 +248,22 @@ export class EffectTransfer {
                 title: game.i18n.localize("ET.Dialog.Title"),
             });
         }
-        
+
         // Get the relevant part from the dialogue return value
         effect_target = effect_target["buttons"];
         EffectTransfer.debug(effect_target);
-        
+
         // If we selected to do nothing do nothing!
-        if ( effect_target === "none" || !effect_target ) return;
-        
+        if (effect_target === "none" || !effect_target) return;
+
         // Reformat our active effects so we can pass them to warpgate later:
-        
+
         /* Bring effects into usable form */
         const updates = EffectTransfer.packageEffects(validEffectsData);
         EffectTransfer.debug("Prepared updates");
         EffectTransfer.debug("Going into the switch case");
-        
-        switch ( effect_target ){
+
+        switch (effect_target) {
             case 'selfToken':
                 EffectTransfer.debug("Going into selfToken");
                 EffectTransfer.debug(tokenDoc);
@@ -276,13 +276,13 @@ export class EffectTransfer {
                 EffectTransfer.debug(tokenDoc);
                 /* If we did not find any tokens just create the effects as usual
                 and have the user deal with the extra inconvenience of reverting the change */
-                await actor.createEmbeddedDocuments("ActiveEffect",validEffectsData);
+                await actor.createEmbeddedDocuments("ActiveEffect", validEffectsData);
                 break;
             case 'targets':
                 // If we selected targets option we need to put the effects on targets
                 EffectTransfer.debug(game.user.targets);
                 /* Loop over each target and apply the effect via warpgate */
-                for (let target of game.user.targets){
+                for (let target of game.user.targets) {
                     EffectTransfer.debug(game.user.name);
                     EffectTransfer.debug(target.document.name);
                     EffectTransfer.applyPackagedEffects(target.document, updates, itemName);
@@ -293,13 +293,13 @@ export class EffectTransfer {
                 return;
         }
     }
-    
+
     // gets the actor, item, and token from the item roll, and passes it to EffectTransferDialogue
-    static async parseItemRoll(item, context, options){
+    static async parseItemRoll(item, context, options) {
         let actor = item?.parent;
         // if the item is not embedded it is not useful for us
-        if ( !actor ) return;
-        
+        if (!actor) return;
+
         let tokenDoc = EffectTransfer.tokenDocFromActor(actor);
         let itemName = item?.name ?? "Unknown item";
         EffectTransfer.debug("Function used to filter:", EffectTransfer.isEligible("chat"));
@@ -308,30 +308,30 @@ export class EffectTransfer {
         itemName = item.name;
 
         // If we have nothing to transfer just exit
-        if ( validEffects.length === 0 ) return;
+        if (validEffects.length === 0) return;
 
         EffectTransfer.debug("Effectarray has a length greater than 0", validEffects);
         const validEffectsData = validEffects.map(e => e.toObject());
         return EffectTransfer.effectTransferDialogue(actor, tokenDoc, itemName, validEffectsData);
     }
-    
-    static async EffectTransferTrigger(item, type = "button"){
+
+    static async EffectTransferTrigger(item, type = "button") {
         EffectTransfer.debug(item);
         const actor = item.parent;
         const tokenDoc = EffectTransfer.tokenDocFromActor(actor);
         const validEffects = item.effects.filter(EffectTransfer.isEligible(type));
-        if ( validEffects.length === 0 ){
+        if (validEffects.length === 0) {
             ui.notifications.warn(game.i18n.localize("ET.Button.warn"));
             return;
         }
         const validEffectsData = validEffects.map(e => e.toObject());
         return EffectTransfer.effectTransferDialogue(actor, tokenDoc, item.name, validEffectsData);
     }
-    
+
     // Add the effect transfer button to the item sheet if the item has a non-transfer effect
-    static async EffectTransferButton(app, array){
+    static async EffectTransferButton(app, array) {
         // Only add a button if the item has eligible effects
-        if ( app.object.effects.filter(EffectTransfer.isEligible("button")).length > 0 ){
+        if (app.object.effects.filter(EffectTransfer.isEligible("button")).length > 0) {
             const transferButton = {
                 class: "EffectTransfer",
                 icon: "fas fa-exchange-alt", //https://fontawesome.com/v5.15/icons
@@ -341,12 +341,12 @@ export class EffectTransfer {
             array.unshift(transferButton);
         }
     }
-    
-    static async EffectConfiguration(app, html, hookData){
+
+    static async EffectConfiguration(app, html, hookData) {
         let tickBox = html[0].querySelector('[name="transfer"]');
         const boxLine = tickBox?.closest('div.form-group');
         if (!boxLine) return;
-        
+
         const block = {
             button: foundry.utils.getProperty(hookData, "effect.flags.effective-transferral.transferBlock.button") ?? false,
             chat: foundry.utils.getProperty(hookData, "effect.flags.effective-transferral.transferBlock.chat") ?? false,
@@ -355,7 +355,7 @@ export class EffectTransfer {
             buttonBlockText: game.i18n.localize("ET.AE.config.chatBlock"),
             displayCardBlockText: game.i18n.localize("ET.AE.config.displayCardBlock")
         }
-        
+
         const div = document.createElement("div");
         div.innerHTML = await renderTemplate(`modules/effective-transferral/templates/EffectConfig.html`, block);
         boxLine.after(...div.children);
@@ -364,11 +364,11 @@ export class EffectTransfer {
 
     static createChatLogButtons = (item, messageData, options) => {
         // if disabled, don't create button.
-        if ( !!MODULE.getSetting("neverDisplayCardTransfer") ) return;
+        if (!!MODULE.getSetting("neverDisplayCardTransfer")) return;
 
         // if no valid effects, don't create button.
         const validEffects = item.effects.filter(EffectTransfer.isEligible("displayCard"));
-        if ( !validEffects.length ) return;
+        if (!validEffects.length) return;
 
         // create button.
         const template = document.createElement("DIV");
@@ -393,40 +393,40 @@ export class EffectTransfer {
 
     static triggerTransferFromChatLog = async (event) => {
         let button = event.target?.closest("button[name='ET-TRANSFER-BUTTON']");
-        if ( !button ) return;
+        if (!button) return;
         button.disabled = false;
-        let itemUuid=button.dataset.uuid
+        let itemUuid = button.dataset.uuid
         let item = fromUuidSync(itemUuid);
-        if ( !item ) {
+        if (!item) {
             const messageId = button.closest("li.chat-message")?.dataset.messageId;
             const message = game.messages.get(messageId);
             const itemData = message.getFlag("dnd5e", "itemData");
 
             // filter effects and bail if there's nothing to do
-            let validEffectsData=itemData.effects.filter(EffectTransfer.isEligible("displayCard"))
-            EffectTransfer.debug("Data of valid effects from chat button",validEffectsData)
-            if (validEffectsData.length===0) return ui.notifications.warn(game.i18n.localize("ET.Button.warn"))
-            
-            let itemHolder=fromUuidSync(button.dataset.actorUuid)
+            let validEffectsData = itemData.effects.filter(EffectTransfer.isEligible("displayCard"))
+            EffectTransfer.debug("Data of valid effects from chat button", validEffectsData)
+            if (validEffectsData.length === 0) return ui.notifications.warn(game.i18n.localize("ET.Button.warn"))
+
+            let itemHolder = fromUuidSync(button.dataset.actorUuid)
 
             // Initiliaze variables
             let actor
             let tokenDoc
-            if (itemHolder instanceof Actor){
-                actor=itemHolder
-                tokenDoc=EffectTransfer.tokenDocFromActor(itemHolder)
-            }else if(itemHolder instanceof TokenDocument){
-                tokenDoc=itemHolder;
-                actor=itemHolder.actor
+            if (itemHolder instanceof Actor) {
+                actor = itemHolder
+                tokenDoc = EffectTransfer.tokenDocFromActor(itemHolder)
+            } else if (itemHolder instanceof TokenDocument) {
+                tokenDoc = itemHolder;
+                actor = itemHolder.actor
             }
-            
-            return EffectTransfer.effectTransferDialogue(actor,tokenDoc,itemData.name,validEffectsData)
-        }else{
+
+            return EffectTransfer.effectTransferDialogue(actor, tokenDoc, itemData.name, validEffectsData)
+        } else {
             return EffectTransfer.EffectTransferTrigger(item, "displayCard");
         }
     }
-    
-    static register(){
+
+    static register() {
         Hooks.on("dnd5e.useItem", EffectTransfer.parseItemRoll);
         Hooks.on("getItemSheetHeaderButtons", EffectTransfer.EffectTransferButton);
         Hooks.on("renderActiveEffectConfig", EffectTransfer.EffectConfiguration);
