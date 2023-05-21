2b4c9fc9e4dc0e5a6f77584c625268e6efe06fa9
Kenneth B <kennethzhell@gmail.com>
2023-05-21 21:01:38 +0200

    add `id` for easy reference in `MODULE` class

--- scripts/module.mjs
+++ scripts/module.mjs
@@ -1,17 +1,17 @@
-/* 
+/*
  * This file is part of the Effective Transferral module (https://github.com/GamerFlix/effective-transferral)
  * Copyright (c) 2021 Felix Pohl.
- * 
- * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
  *
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
@@ -38,7 +38,7 @@ const SUB_MODULES = {
 };
 
 const SUB_APPS = {
-  //MyDialog  
+  //MyDialog
 }
 
 /*
@@ -47,11 +47,13 @@ const SUB_APPS = {
 
 export class MODULE {
 
+  static id = "effective-transferral";
+
   static build() {
     //all startup tasks needed before sub module initialization
 
   }
-  
+
   static getSetting(key){
     return game.settings.get("effective-transferral", key)
   }
